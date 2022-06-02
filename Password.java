569edd008fb6aa8ed2c4715e486fffc96663808b
Alexandr Sidorov <an.sidorov@i-treco.ru>
2022-06-02 20:51:07 +0300

    home working is fine 3

--- Password.java
+++ Password.java
@@ -1,13 +1,12 @@
 public class Password {
     public static void main(String[] args) {
-        String password = "1";
+        String password = "Caput Draconis1";
         String truepassword = "Caput Draconis";
 
- if(password == truepassword){
-     System.out.println("Welcome");
- }
- else {
-     System.out.println("access denied");
- }
+        if (password.equals(truepassword)) {
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Access denied");
+        }
     }
 }
