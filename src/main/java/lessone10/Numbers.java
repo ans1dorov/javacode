5349c757ff809c57eb94bc43ff3dab0084c69d4d
Alexandr Sidorov <an.sidorov@i-treco.ru>
2022-08-16 12:47:16 +0300

    homework from lecture 10-2

--- src/main/java/lessone10/Numbers.java
+++ src/main/java/lessone10/Numbers.java
@@ -18,6 +18,8 @@ import java.util.ListIterator;
 public class Numbers {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+        System.out.println("average(numbers) = " + average(numbers));
+        printInRange(numbers, 1, 11);
     }
 
     private static double average(List<Integer> numbers) {
